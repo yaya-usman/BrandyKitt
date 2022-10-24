c13ed1224315b770518abfacb68c5bdd5e20a110
yaya-usman <yayaazeez222@gmail.com>
2022-10-25 00:24:03 +0300

    keyword generation method added

--- app/brandykitt.py
+++ app/brandykitt.py
@@ -1,6 +1,8 @@
 import os
+from typing import List
 import openai
 import argparse
+import re
 from dotenv import load_dotenv
 
 
@@ -10,10 +12,13 @@ def main():
     args = parser.parse_args()
     user_input = args.input
 
-    generate_branding_snippet(user_input)
+    branding_result = generate_branding_snippet(user_input)
+    keywords_result = generate_keywords(user_input)
+    print(branding_result)
+    print(keywords_result)
 
 
-def generate_branding_snippet(subject: str):
+def generate_branding_snippet(subject: str) -> str:
 
     # Load your API key from an environment variable or secret management service
     openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -21,13 +26,35 @@ def generate_branding_snippet(subject: str):
 
     response = openai.Completion.create(
         model="text-davinci-002", prompt=prompt, temperature=0, max_tokens=35)
-    branding_text = response["choices"][0]["text"]
+    
+    #Extracting and cleaning text from response 
+    branding_text: str = response["choices"][0]["text"]
+    branding_text = branding_text.strip()
+
     last_char = branding_text[-1]
 
     if last_char not in {".", "!", "?"}:
         branding_text += "..."
 
-    print(branding_text)
+    return branding_text
+
+
+def generate_keywords(subject: str) -> List[str]:
+
+    # Load your API key from an environment variable or secret management service
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    prompt = f"Generate unique related branding keywords for {subject}"
+
+    response = openai.Completion.create(
+        model="text-davinci-002", prompt=prompt, temperature=0, max_tokens=35)
+    
+    #Extracting and cleaning text from response 
+    keywords_text: str = response["choices"][0]["text"]
+    keywords_text = keywords_text.strip()
+    keywordsList = re.split(",|-|\n", keywords_text)
+    keywordsList = [k.lower().strip() for k in keywordsList if len(k) > 0]
+
+    return keywordsList
 
 
 if __name__ == "__main__":

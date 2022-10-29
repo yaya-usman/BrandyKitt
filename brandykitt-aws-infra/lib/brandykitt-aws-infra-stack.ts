f24d4285902e78d5e26b3a7ecbfd5d301edb0894
yaya-usman <yayaazeez222@gmail.com>
2022-10-29 19:46:08 +0300

    fixed deps error

--- brandykitt-aws-infra/lib/brandykitt-aws-infra-stack.ts
+++ brandykitt-aws-infra/lib/brandykitt-aws-infra-stack.ts
@@ -1,7 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
-import * as dotenv from 'dotenv'
+import * as dotenv from 'dotenv';
+import { Duration } from 'aws-cdk-lib';
 
 dotenv.config();
 
@@ -11,17 +12,18 @@ export class BrandykittAwsInfraStack extends cdk.Stack {
 
     const layer = new lambda.LayerVersion(this, 'BaseLayer', {
       code: lambda.Code.fromAsset("lambda_base_layer/layer.zip"),
-      compatibleRuntimes: [lambda.Runtime.PYTHON_3_9],
+      compatibleRuntimes: [lambda.Runtime.PYTHON_3_7],
     })
 
     const apiLambda = new lambda.Function(this, 'BrandykittLambdaFunction', {
-      runtime: lambda.Runtime.PYTHON_3_9,
+      runtime: lambda.Runtime.PYTHON_3_7,
       code: lambda.Code.fromAsset("../app/"),
       handler: "brandykitt_api.handler",
       layers: [layer],
       environment: {
-        OPENAI_API_KEY: process.env.OPENAI_API_KEY ?? "",
-      }
+        "OPENAI_API_KEY": process.env.OPENAI_API_KEY!,
+      },
+      timeout: Duration.seconds(30),
     });
   }
 }
